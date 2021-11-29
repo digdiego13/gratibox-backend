90f2295cf5c1819ad9f87bb2899e358e39e51f66
Diego Mamede Nogueira <diegomamede@poli.ufrj.br>
2021-11-29 13:05:00 -0300

    Fazendo deploy Heroku

--- src/setup.js
+++ src/setup.js
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 
 let envFile = '.env.test';
 
+if (process.env.NODE_ENV === 'prod') {
+  envFile = '.env';
+}
+
 if (process.env.NODE_ENV === 'dev') {
   envFile = '.env.dev';
 }
